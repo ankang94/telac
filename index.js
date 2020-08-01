309280232587f5fff43fbc176aff6129353917e9
ankang94 <37808034+ankang94@users.noreply.github.com>
2020-08-02 00:38:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const configs = {
     sw2: '192.168.10.1'
 };
 
+function welcome() {
+    let output = '\n';
+    output += '___________    .__                 __        \n';
+    output += '\\__    ___/___ |  |   ____   _____/  |_     \n';
+    output += '  |    |_/ __ \\|  |  /    \\_/ __ \\   __\\ \n';
+    output += '  |    |\\  ___/|  |_|   |  \\  ___/|  |     \n';
+    output += '  |____| \\___  >____/___|  /\\___  >__|     \n';
+    output += '             \\/          \\/     \\/        \n';
+    console.log(output);
+}
+
 program
     .version('0.0.1')
     .requiredOption('-u, --user <string>', 'username');
@@ -45,12 +56,15 @@ let obv = inquirer.prompt(prompts);
 obv.ui.process.subscribe(function (answer) {
     if (answer.name === 'device' && answer.answer === 'kill') {
         obv.ui.rl.output.write(ansiEscapes.eraseLines(200));
+        welcome();
         prompts.next(killdev);
     } else if (answer.name === 'killdev') {
         obv.ui.rl.output.write(ansiEscapes.eraseLines(200));
+        welcome();
         console.log(`killing ${answer.answer}, please wait...`);
         setTimeout(function () {
             obv.ui.rl.output.write(ansiEscapes.eraseLines(200));
+            welcome();
             prompts.next(devices);
         }, 2000);
     } else {
@@ -58,9 +72,9 @@ obv.ui.process.subscribe(function (answer) {
         connect(answer.answer);
     }
 }, function (error) {
-    console.log(error)
+    process.exit(error.code);
 }, function () {
-
+    process.exit();
 });
 
 obv.ui.rl.listeners("SIGINT").forEach(listener => obv.ui.rl.off("SIGINT", listener));
@@ -68,6 +82,7 @@ obv.ui.rl.on("SIGINT", () => {});
 obv.ui.rl.on("SIGTSTP", () => {});
 
 obv.ui.rl.output.write(ansiEscapes.eraseLines(200));
+welcome();
 prompts.next(devices);
 
 function connect(name) {
@@ -83,6 +98,7 @@ function connect(name) {
     ptyProcess.on('exit', function () {
         obv.ui.rl.output.unmute();
         obv.ui.rl.output.write(ansiEscapes.eraseLines(200));
+        welcome();
         prompts.next(devices);
     });
 
